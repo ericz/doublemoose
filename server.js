abe738e5b6e219f872ededfcbee6d1bfce8b0993
ericz <really.ez@gmail.com>
2014-04-27 23:15:17 -0700

    Add git pull route

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+// Toggle screen brightness
 app.post('/lights', function(req, res){
-  var script = spawn('./AutoIt3.exe', ['scripts/als.au3']);
-  res.send('ok')
+  spawn('./AutoIt3.exe', ['scripts/als.au3']);
+  res.send(200);
+});
+
+// Git pull self
+app.post('/gitPull', function(req, res){
+  res.send(200);
+  var script = spawn('git pull');
 });
 
 app.listen(80);
